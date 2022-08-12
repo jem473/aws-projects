111626b7335160ad4d4e86386e5796d27e522600
Jonathan Monreal <me@jonathanmonreal.com>
2022-08-11 18:04:04 -0600

    add variable descriptions for vars with default values

--- s3-events-example-1-tf/main.tf
+++ s3-events-example-1-tf/main.tf
@@ -24,6 +24,7 @@ variable "destination_bucket_name" {
 variable "filter_suffix" {
   type = string
   default = ".flac"
+  description = "The suffix to filter on for the S3 Event notification."
 }
 
 variable "lambda_filename" {
@@ -39,19 +40,23 @@ variable "ffmpeg_layer_filename" {
 variable "lambda_memory" {
   type = number
   default = 1024
+  description = "Memory to allocate to the Lambda function, in MB."
 }
 
 variable "lambda_ephemeral_storage" {
   type = number
   default = 512
+  description = "Ephemeral storage to allocate to the Lambda function, in MB."
 }
 
 variable "lambda_timeout" {
   type = number
   default = 120
+  description = "The timeout for the Lambda function, in seconds."
 }
 
 variable "region" {
   type = string
   default = "us-east-1"
+  description = "The region in which the S3 buckets will be created and the Lambda function will be executed."
 }
