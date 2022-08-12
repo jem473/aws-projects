d67af605d9dc732f6e7176ffbd350bda64b55556
Jonathan Monreal <me@jonathanmonreal.com>
2022-08-12 14:07:17 -0600

    add aws_dms_replication_instance

--- dms-mongodb-to-dynamodb-example-1/main.tf
+++ dms-mongodb-to-dynamodb-example-1/main.tf
@@ -16,3 +16,43 @@ variable "region" {
   default = "us-east-1"
   description = "The region in which the replication instance will be created."
 }
+
+variable "replication_instance_availability_zone" {
+  type = string
+  default = null
+}
+
+variable "replication_instance_multi_az" {
+  type = bool
+  default = false
+}
+
+variable "replication_instance_preferred_maintenance_window" {
+  type = string
+  default = "sun:9:00-sun:11:00"
+}
+
+variable "replication_instance_class" {
+  type = string
+  default = "dms.t2.micro"
+}
+
+variable "replication_instance_id" {
+  type = string
+  default = "mongodb-to-dynamodb-test"
+}
+
+variable "dms_engine_version" {
+  type = string
+  default = "3.1.4"
+}
+
+variable "dms_kms_key_arn" {
+  type = string
+  default = null
+}
+
+variable "replication_instance_allocated_storage" {
+  type = number
+  default = 8
+}
