d626a13cc7d404140eaa79df27ed2cff996b93da
Jonathan Monreal <me@jonathanmonreal.com>
2022-08-12 13:28:55 -0600

    correct description for use of region var

--- dms-mongodb-to-dynamodb-example-1/main.tf
+++ dms-mongodb-to-dynamodb-example-1/main.tf
@@ -14,5 +14,5 @@ provider "aws" {
 variable "region" {
   type = string
   default = "us-east-1"
-  description = "The region in which the S3 buckets will be created and the Lambda function will be executed."
+  description = "The region in which the replication instance will be created."
 }
